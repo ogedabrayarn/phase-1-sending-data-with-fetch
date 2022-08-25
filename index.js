66983c9351699f027c0e93fc66cd3b7dbb35e106
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-08-26 01:15:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(userName, email) {
+      const users = {
+            name: userName,
+            email: email
+      };
+      return fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                  'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(users)
+      })
+            .then(res => res.json())
+            .then(data => {
+                  console.log(data);
+            })
+            .catch(message => {
+                  document.querySelector('p').innerHTML = message;
+            });
+
+}
+console.log(submitData('Samuel', 'sammysam@hotmail.com'))
